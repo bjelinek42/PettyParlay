b464234e5e5ac6e6cee512b4cb02ac01d68bd08e
ben <bjelinek42@gmail.com>
2022-11-28 13:26:09 -0700

    working db schema and associations for friends

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_28_190953) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_28_201323) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "friendship", force: :cascade do |t|
+  create_table "friendships", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.bigint "friends_id"
+    t.bigint "friend_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["friends_id"], name: "index_friendship_on_friends_id"
-    t.index ["user_id"], name: "index_friendship_on_user_id"
+    t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,6 +31,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_28_190953) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "friendship", "users"
-  add_foreign_key "friendship", "users", column: "friends_id"
+  add_foreign_key "friendships", "users"
+  add_foreign_key "friendships", "users", column: "friend_id"
 end
