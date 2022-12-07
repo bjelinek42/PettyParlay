2f5fb40085d9674f3ca25770c027d72d729f5c3e
ben <bjelinek42@gmail.com>
2022-12-07 14:43:29 -0700

    friendship create action

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -5,4 +5,18 @@ class FriendshipsController < ApplicationController
     render json: friendships
   end
 
+  def show
+
+  end
+
+  def create
+    friendship = Friendship.new_friendship(params, current_user.id)
+    if friendship.save
+      render json: { message: "You are now friends" }, status: :created
+    else
+      render json: { errors: friendship.errors.full_messages }, status: :bad_request
+    end
+  end
+
+
 end
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,4 +3,13 @@ class Friendship < ApplicationRecord
   belongs_to :friend, class_name: "User"
   has_many :bets_friendships
   has_many :bets, through: :bets_friendships
+
+  def self.new_friendship(params, current_user_id)
+    friendship = Friendship.new(
+      user_id: current_user_id,
+      friend_id: params[:friend_id]
+    )
+    return friendship
+  end
+
 end
