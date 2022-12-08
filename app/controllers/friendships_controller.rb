c304d440be4bbce0af9413665a3033b3adedb219
ben <bjelinek42@gmail.com>
2022-12-08 13:53:05 -0700

    friendship destroy action

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -18,5 +18,14 @@ class FriendshipsController < ApplicationController
     end
   end
 
+  def destroy
+    friendship = Friendship.find_by(id: params[:id])
+    if friendship.destroy
+      render json: { message: "You are nolonger friends" }, status: :created
+    else
+      render json: { errors: friendship.errors.full_messages }, status: :bad_request
+    end
+  end
+
 
 end
